1ca599c2ec7ef1e74a0cf906886239242aeb388b
joao.santos <joao.santos@flexpag.com>
2022-07-25 11:26:19 -0300

    Adicionando projeto spring

---
+++ src/main/java/com/flexpag/paymentscheduler/PaymentSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.flexpag.paymentscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flexpag/paymentscheduler/PaymentSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flexpag.paymentscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
