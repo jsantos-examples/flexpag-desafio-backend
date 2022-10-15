c87d414c609130f2340f3c8628827bd959f9f04d
Wellington José <paivawellington23@gmail.com>
2022-10-15 16:04:28 -0300

    feat: added documentation swagger-ui

--- src/main/java/com/flexpag/paymentscheduler/PaymentSchedulerApplication.java
+++ src/main/java/com/flexpag/paymentscheduler/PaymentSchedulerApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @RestController
 @EnableScheduling
+@EnableWebMvc
 public class PaymentSchedulerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentSchedulerApplication.class, args);
