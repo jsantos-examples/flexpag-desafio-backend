916d7edbb08948101c16c75922a11265565cf6b1
Marina Varela <marinavareladev@gmail.com>
2023-03-06 00:08:32 -0300

    Implementação no repositório

--- src/main/java/com/flexpag/paymentscheduler/repository/PaymentRepository.java
+++ src/main/java/com/flexpag/paymentscheduler/repository/PaymentRepository.java
@@ -2,13 +2,17 @@ package com.flexpag.paymentscheduler.repository;
 
 import java.util.List;
 
+//import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.flexpag.paymentscheduler.model.PaymentModel;
+import com.flexpag.paymentscheduler.model.status.PaymentStatus;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<PaymentModel, Long> {
 		public List<PaymentModel> findAllById (Long id);
+		public List<PaymentModel> findByStatus(PaymentStatus status);
 
 }
